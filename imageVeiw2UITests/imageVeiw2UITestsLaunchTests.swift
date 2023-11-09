82e659d6f81d95b51a2de5fbd60340117f5ae44e
yejun <tlsdpwns0612@naver.com>
2023-11-09 09:16:11 +0900

    Initial Commit

---
+++ imageVeiw2UITests/imageVeiw2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  imageVeiw2UITestsLaunchTests.swift
+//  imageVeiw2UITests
+//
+//  Created by 신예준 on 11/9/23.
+//
+
+import XCTest
+
+final class imageVeiw2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
